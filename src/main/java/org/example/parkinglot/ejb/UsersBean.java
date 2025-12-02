64896d840ab98386efc33fe8bae02b3f4bd009c2
Mircea9 <gmircea.12@gmail.com>
2025-12-02 17:32:44 +0200

    Added log in and usergroups

--- src/main/java/org/example/parkinglot/ejb/UsersBean.java
+++ src/main/java/org/example/parkinglot/ejb/UsersBean.java
@@ -3,19 +3,25 @@ package org.example.parkinglot.ejb;
 
 import jakarta.ejb.EJBException;
 import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.example.parkinglot.common.UserDto;
 import org.example.parkinglot.entities.Car;
 import org.example.parkinglot.entities.User;
+import org.example.parkinglot.entities.UserGroup;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
 @Stateless
 public class UsersBean {
+    @Inject
+    PasswordBean passwordBean;
+
     private static final Logger LOG = Logger.getLogger(UsersBean.class.getName());
 
     @PersistenceContext
@@ -44,6 +50,25 @@ public class UsersBean {
             return copyUsersToDto(users);
         } catch (Exception ex) {
             throw new EJBException(ex);
+        }
+    }
+
+    public void createUser(String username, String email, String password, Collection<String> groups) {
+        LOG.info("createUser");
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+        newUser.setPassword(passwordBean.convertToSha256(password));
+        entityManager.persist(newUser);
+        assignGroupsToUser(username, groups);
+    }
+    private void assignGroupsToUser(String username, Collection<String> groups) {
+        LOG.info("assignGroupsToUser");
+        for (String group : groups) {
+            UserGroup userGroup = new UserGroup();
+            userGroup.setUsername(username);
+            userGroup.setUserGroup(group);
+            entityManager.persist(userGroup);
         }
     }
 
